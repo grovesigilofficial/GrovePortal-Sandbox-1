1005e9ae19cb900f70f63e4e021cf3c0d71ab903
GroveSigilOfficial <grovesigilofficial@gmail.com>
2025-12-03 06:57:39 -0800

    Create config.local.js

---
+++ src/assets/js/config.local.js
@@ -0,0 +1,5 @@
+export const CONFIG = {
+  LOCAL_COUNTER_KEY: "grove_uberman_day",
+  adminUsername: "Bleedvow",          // change to your admin username
+  adminPassword: "S0m3$tr0ngP@ssw0rd" // change to your admin password
+};
